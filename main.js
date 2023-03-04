e3cbddc012e600d6651d4903a3ead36dbb63040e
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-03-04 18:22:47 -0500

    Added objects to array

---
+++ main.js
@@ -0,0 +1,20 @@
+let myLibrary = []
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+
+function addBookToLibrary() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    console.log(myLibrary)
+}
+
