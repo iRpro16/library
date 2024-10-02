4d94e779892e1ea5e469358eff14e53fec119e80
iRpro16 <rumeoisidoro16@gmail.com>
2024-10-01 23:02:59 -0400

    rough draft

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const addBook = document.querySelector('.add-book');
+const showDialog = document.getElementById('dialog');
+const confirmBtn = document.getElementById('confirmBtn');
+const authorName =  document.getElementById('author');
+const titleBook = document.getElementById('title');
+const pages = document.getElementById('numPages');
+const statusBook = document.getElementById('status');
 const myLibrary = [];
 
-function Book() {
-    // the constructor...
+function Book(author, title, numPages, status) {
+    this.author = author;
+    this.title = title;
+    this.numPages = numPages;
+    this.status = status;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(author, title, numPages, status) {
+    let book = new Book(author.value, title.value, numPages.value, status);
+    myLibrary.push(book);
+    console.log(myLibrary)
+
 }
+
+addBook.addEventListener("click", () => showDialog.showModal());
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    showDialog.close();
+    addBookToLibrary(authorName, titleBook, pages, statusBook);
+})
