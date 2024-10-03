1a89a260d293b3c88b26bc83defadec1ea3e2c1a
iRpro16 <rumeoisidoro16@gmail.com>
2024-10-02 21:38:56 -0400

    user input stored and pushed to array

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const pages = document.getElementById('numPages');
 const statusBook = document.getElementById('status');
 const myLibrary = [];
 
+// Constructor for books
 function Book(author, title, numPages, status) {
     this.author = author;
     this.title = title;
@@ -14,16 +15,42 @@ function Book(author, title, numPages, status) {
     this.status = status;
 }
 
-function addBookToLibrary(author, title, numPages, status) {
-    let book = new Book(author.value, title.value, numPages.value, status);
+// Push new books to array
+function addBookToLibrary() {
+    let book = new Book(authorName.value, titleBook.value, pages.value, statusBook.value);
     myLibrary.push(book);
+    clearVars();
     console.log(myLibrary)
-
 }
 
+// Open modal
 addBook.addEventListener("click", () => showDialog.showModal());
+
+// Confirm button modal to submit user input
 confirmBtn.addEventListener("click", (event) => {
     event.preventDefault();
     showDialog.close();
-    addBookToLibrary(authorName, titleBook, pages, statusBook);
+    addBookToLibrary();
 })
+
+// toggle button
+function toggleLabel() {
+    if (statusBook.textContent === 'Not Read') {
+        statusBook.textContent = 'Read';
+        statusBook.value = 'Read';
+        statusBook.style.backgroundColor = 'lightgreen';
+    } else {
+        statusBook.textContent = 'Not Read';
+        statusBook.value = 'Not Read';
+        statusBook.style.backgroundColor = '#FFCCCB';
+    }
+}
+
+// Clear modal everytime
+function clearVars() {
+    authorName.value = '';
+    titleBook.value = '';
+    pages.value = '';
+    statusBook.value = 'Not Read';
+    statusBook.style.backgroundColor = "#FFCCCB";
+}
