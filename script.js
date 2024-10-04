7048ca6030ec448b1e0f8debea9a2603391c72ba
iRpro16 <rumeoisidoro16@gmail.com>
2024-10-03 20:40:22 -0400

    managed to append last element of array only

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
+const myLibrary = [];
+
 const addBook = document.querySelector('.add-book');
 const showDialog = document.getElementById('dialog');
 const confirmBtn = document.getElementById('confirmBtn');
 const authorName =  document.getElementById('author');
 const titleBook = document.getElementById('title');
-const pages = document.getElementById('numPages');
+const numPages = document.getElementById('numPages');
 const statusBook = document.getElementById('status');
-const myLibrary = [];
 
 // Constructor for books
-function Book(author, title, numPages, status) {
+function Book(author, title, pages, status) {
     this.author = author;
     this.title = title;
-    this.numPages = numPages;
+    this.pages = pages;
     this.status = status;
 }
 
 // Push new books to array
 function addBookToLibrary() {
-    let book = new Book(authorName.value, titleBook.value, pages.value, statusBook.value);
+    let book = new Book(authorName.value, titleBook.value, numPages.value, statusBook.value);
     myLibrary.push(book);
     clearVars();
-    console.log(myLibrary)
+    console.log(myLibrary);
+}
+
+function createBook() {
+    const mainContent = document.querySelector(".main-content");
+    mainContent.innerHTML = "";
+    myLibrary.forEach((book)=> {
+
+        // Create card
+        let newCard = document.createElement("div");
+        newCard.classList.add("card");
+        mainContent.append(newCard);
+
+        // Get properties
+        let cardTitle = book.title;
+        let cardAuthor = book.author;
+        let cardPages = book.pages;
+        let cardStatus = book.status;
+
+        newCard.append(
+            cardTitle,
+            cardAuthor,
+            cardPages,
+            cardStatus
+        )
+    })
+
 }
 
 // Open modal
@@ -31,26 +58,12 @@ confirmBtn.addEventListener("click", (event) => {
     event.preventDefault();
     showDialog.close();
     addBookToLibrary();
+    createBook();
 })
-
-// toggle button
-function toggleLabel() {
-    if (statusBook.textContent === 'Not Read') {
-        statusBook.textContent = 'Read';
-        statusBook.value = 'Read';
-        statusBook.style.backgroundColor = 'lightgreen';
-    } else {
-        statusBook.textContent = 'Not Read';
-        statusBook.value = 'Not Read';
-        statusBook.style.backgroundColor = '#FFCCCB';
-    }
-}
 
 // Clear modal everytime
 function clearVars() {
     authorName.value = '';
     titleBook.value = '';
-    pages.value = '';
-    statusBook.value = 'Not Read';
-    statusBook.style.backgroundColor = "#FFCCCB";
+    numPages.value = '';
 }
